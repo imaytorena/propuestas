8753d1b3080dfea9282753efeaeabc92bef41269
Israel Maytorena <i.velazquez@cignuz.com>
2025-01-26 22:26:14 -0600

    FIX using env inside prisma

--- src/lib/server/db/prisma.ts
+++ src/lib/server/db/prisma.ts
@@ -1,7 +1,14 @@
 // import { PrismaClient } from '@prisma/client'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { DATABASE_URL } from '$env/static/private';
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+const prisma = new PrismaClient({
+	datasources: {
+		db: {
+			url: DATABASE_URL
+		}
+	}
+}).$extends(withAccelerate());
 
-export default prisma
+export default prisma;
