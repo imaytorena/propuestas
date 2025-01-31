8be4b7ace789a17f7ea240fa27a4b1ae60e17c2e
Israel Maytorena <i.velazquez@cignuz.com>
2025-01-30 20:17:46 -0600

    moving to node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,17 @@
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '200.html',
+			// you can override defaults here
+			out: 'build',
 			precompress: false,
-			strict: true
+			envPrefix: ''
 		})
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
