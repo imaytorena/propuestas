80fb407bda88c1b94e95a9affe76032bd3ee87b6
Israel Maytorena <i.velazquez@cignuz.com>
2025-01-30 19:30:12 -0600

    Removing vite config

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,6 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
 			// default options are shown. On some platforms
