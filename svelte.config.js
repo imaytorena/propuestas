8474635c804ef27ecf26db76fb5efcf2de79fdd1
Israel Maytorena <i.velazquez@cignuz.com>
2025-01-30 20:04:25 -0600

    Adding some fixes

--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,13 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		prerender: vitePreprocess(),
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '200.html',
 			precompress: false,
 			strict: true
 		})
