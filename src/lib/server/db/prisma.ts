7b13d1b0ccef6c79c6b6f6c7ad505e8bbb10c813
Israel Maytorena <i.velazquez@cignuz.com>
2025-01-30 19:18:51 -0600

    Updating adapter

--- src/lib/server/db/prisma.ts
+++ src/lib/server/db/prisma.ts
@@ -1,12 +1,12 @@
 // import { PrismaClient } from '@prisma/client'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { DATABASE_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 const prisma = new PrismaClient({
 	datasources: {
 		db: {
-			url: DATABASE_URL
+			url: env.DATABASE_URL
 		}
 	}
 }).$extends(withAccelerate());
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,22 +8,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
+		// Otras configuraciones...
+		env: {
+			public: {},
+			private: {
+				DIRECT_URL: process.env.DIRECT_URL, // Para el servidor
+				DATABASE_URL: process.env.DATABASE_URL // Para el servidor
 			}
-		})
+		}
 	}
 };
 
