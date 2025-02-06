03b1cb47efed5b08739b2462176db1db94eae38f
Israel Maytorena <i.velazquez@cignuz.com>
2025-02-06 11:19:21 -0600

    Adapter auto in config

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// you can override defaults here
-			out: 'build',
-			precompress: false,
-			envPrefix: '',
-			polyfill: true
-		}),
+		adapter: adapter({}),
 		files: {
 			hooks: {
 				server: 'src/hooks.server'
